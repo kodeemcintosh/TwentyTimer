131edc65f4f3d4e18d1430f669df31c05874cc2a
kvmac <kodee.mcintosh@gmail.com>
2018-08-09 01:32:15 -0400

    Initial commit with most of the application built out

---
+++ main.js
@@ -0,0 +1,54 @@
+
+const {app, BrowserWindow} = require('electron');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    title: 'DevTimer',
+    icon: __dirname + '/img/icon.png',
+    width: 400,
+    height: 300});
+  // mainWindow.backgroundColor();
+  mainWindow.setMenu(null);
+
+  mainWindow.loadFile('index.html');
+
+  mainWindow.show(); 
+
+  mainWindow.focus(); 
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
+
+app.on('activate', function () {
+
+  if (mainWindow === null) {
+    createWindow()
+  }
+});
+
+app.on('alert', function () {
+  alert = new BrowserWindow({center: true, resizable: false, width: 200, height: 150});
+
+  alert.loadFile('alert.html');
+
+  alert.on('dismiss', function () {
+    alert = null;
+  });
+});
+
+
